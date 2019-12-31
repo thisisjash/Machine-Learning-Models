f13bc190ba998fd6da2475e93107bdc4dd1db303
thisisjash <thisisjash619@gmail.com>
2019-12-31 17:58:50 +0530

    Created KNN Folder

---
+++ K-Nearest-Neighbours/main.py
@@ -0,0 +1,9 @@
+from sklearn import preprocessing
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+
+data = pd.read_csv("car.data")
+print(data.head())
